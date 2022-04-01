02e803558bae1a3c41e67a769853a1f00147af65
Mircea-Gelu Egry <mirceaegry72@gmail.com>
2022-04-01 13:47:25 +0100

    code running video

---
+++ assignment/src/test/java/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+	public static void generatedTests() {
+		TestMethods.testBranchPredicate(378172973,-429947801,2002910391,-32419323);
+		TestMethods.testBranchPredicate(-1655900608,-2033293201,-395630853,44563064);
+		TestMethods.testBranchPredicate(0,75,-67,-5);
+		TestMethods.testBranchPredicate(0,-79,-24,-49);
+		TestMethods.testBranchPredicate(0,-93,92,13);
+		TestMethods.testBranchPredicate(0,62,49,-37);
+	}
+}
---
+++ assignment/src/test/java/Test2.java
@@ -0,0 +1,6 @@
+public class Test2 {
+	public static void generatedTests() {
+		TestMethods.checkIfNumberBigger(593548448,-338150164);
+		TestMethods.checkIfNumberBigger(-1772821524,1150472993);
+	}
+}
