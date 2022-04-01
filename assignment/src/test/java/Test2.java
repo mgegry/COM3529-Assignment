da35211e121b47b6ea7934df7846d2484d71bbee
Mircea-Gelu Egry <mirceaegry72@gmail.com>
2022-04-01 13:48:26 +0100

    deleted created tests

--- assignment/src/test/java/Test2.java
+++
@@ -1,6 +0,0 @@
-public class Test2 {
-	public static void generatedTests() {
-		TestMethods.checkIfNumberBigger(593548448,-338150164);
-		TestMethods.checkIfNumberBigger(-1772821524,1150472993);
-	}
-}
